f3374b41fab67848d57cf91a310546a68942c2fd
unknown <gibmac@outlook.com>
2022-07-26 21:27:29 -0600

    final merge before frontend deploy

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,15 @@ app.use(function(req, res, next) {
   next();
 }); */
 
+//
+if(process.env.NODE_ENV === 'production') {
+  app.use(Express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
