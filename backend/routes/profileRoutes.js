3ebf00fce9b0c12808b4f2ff691a781ee9ead347
unknown <gibmac@outlook.com>
2022-07-26 23:24:41 -0600

    deleted profile routing

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const {requiresAuth} = require('express-openid-connect');
-const express = require('express');
-const router = express.Router();
-const { getProfile, landingPage, dashBoardPage} = require('../controllers/profileController')
-
-//Specialty Routes
-//Profile
-
-module.exports = router;
