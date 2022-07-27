ac94f963326b120717c99f2d4baeb1a6bd66872a
unknown <gibmac@outlook.com>
2022-07-26 22:46:56 -0600

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(function(req, res, next) {
 
 //
 if(process.env.NODE_ENV === 'production') {
-  app.use(Express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
 } else {
